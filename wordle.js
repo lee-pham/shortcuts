da1c0253da04363a55d634db9b24126e6124cc5e
Lee Pham <lee-pham@users.noreply.github.com>
2022-01-20 01:33:51 -0600

    Create wordle.js

---
+++ wordle.js
@@ -0,0 +1,44 @@
+evaluations = [
+    [
+      "correct",
+      "absent",
+      "correct",
+      "correct",
+      "correct"
+    ],
+    [
+      "correct",
+      "correct",
+      "correct",
+      "correct",
+      "correct"
+    ],
+    null,
+    null,
+    null,
+    null
+  ]
+
+evaluations = document.getElementsByTagName("game-app")[0].evaluations
+puzzleNumber = document.getElementsByTagName("game-app")[0].dayOffset
+out = ""
+attempts = 0
+evaluations.forEach((row) => {
+    if (row == null) {
+        return;
+    }
+    attempts += 1
+    row.forEach((element) => {
+        if (element == "correct") {
+            out += "🟩"
+        } else if (element == "absent") {
+            out += "⬛"
+        } else if (element == "present") {
+            out += "🟨"
+        }
+    })
+    out += "\n"
+})
+
+console.log(`Wordle ${puzzleNumber} ${attempts}/6`)
+console.log(out)
